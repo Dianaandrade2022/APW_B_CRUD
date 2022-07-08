fd5fb63e38b4cbda33d191821a91a4ce9964241d
Diana Martinez Andrade <a3521110157@alumno.uttehuacan.edu.mx>
2022-07-07 22:27:41 -0500

    Plantilla actualizada

---
+++ config/conexion_dma.php
@@ -0,0 +1,18 @@
+<?php
+class Conectar{
+    protected $dbh;
+
+    protected function Conexion (){
+        try{
+            $conectar = $this -> dbh = new PDO("mysql:localhost;dbname=apw_b_crud","root","");
+            return $conectar;
+        } catch(Exception $e){
+            print"Error en la conexcion de la base de datos"; $e -> getMessage(), "</br>";
+        }
+    }
+    public function set_names (){
+        return $this -> dbh -> query("SET NAMES ");
+    }
+}
+
+?>
